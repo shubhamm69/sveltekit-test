59797575d4363df872fdcad82a4ca3584b3ff5de
shubhamm69 <109853624+shubhamm69@users.noreply.github.com>
2023-08-30 00:28:11 +0530

    Search minor error fixed

--- src/components/ContactList.svelte
+++ src/components/ContactList.svelte
@@ -4,6 +4,8 @@
   import Card from "./Card.svelte";
 
   let contacts: Contact[] = [];
+  let searchInput = '';
+  let filteredContacts: Contact[] = [];
 
   interface Contact {
     id: number;
@@ -16,21 +18,66 @@
 
   fetchContacts().then((data) => {
     contacts = data;
+    filteredContacts = contacts;
     saveContacts(contacts);
   });
+
+  function updateFilteredContacts() {
+    filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+  }
 </script>
 
-<div class="contact-list">
-  {#each contacts as contact (contact.id)}
-    <Card {contact} />
-  {/each}
-</div>
-
 <style>
+  :global(body) {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  .container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+
+  .search-bar {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+
+  .search-bar input {
+    flex: 1;
+    padding: 10px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+
   .contact-list {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
     gap: 20px;
-    padding: 20px;
   }
+  
 </style>
+
+<div class="container">
+  <div class="search-bar">
+    <input type="text" placeholder="Search contacts..." bind:value={searchInput} on:input={updateFilteredContacts} />
+  </div>
+
+  <div class="contact-list">
+    {#if filteredContacts.length > 0}
+      {#each filteredContacts as contact (contact.id)}
+        <div class="card">
+          <Card {contact} />
+        </div>
+      {/each}
+    {:else}
+      <p>No contacts found.</p>
+    {/if}
+  </div>
+</div>
